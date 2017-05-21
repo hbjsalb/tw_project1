2294222cb4c3a2937d8e16ab9b1f02390d6d0a6a
Lucian <l.lucian_94@yahoo.com>
2017-05-21 21:08:35 +0300

    home page and stats page

--- deton/Statistics.php
+++ deton/Statistics.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+include 'Connection.php';
+$sqlTotalC = "SELECT COUNT(*) AS TOTALC FROM convicts";
+$sqlTotalU = "SELECT COUNT(*) AS TOTALU FROM users";
+$sqlTotalV = "SELECT COUNT(*) AS TOTALV FROM visit WHERE Status = 'Y'";
+$sqlTotalM = "SELECT COUNT(*) AS TOTALM FROM messages";
 
 ?>
 
@@ -8,6 +13,7 @@ session_start();
 <head>
 	<title>Statistics</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="stats.css">
 	<meta charset="UTF-8">
 </head>
 <body>
@@ -16,10 +22,78 @@ include 'OwnFunctions.php';
 if ($_SESSION["role"] == $_roleClient)
 {
 	include_once 'Navbar.php';	
+	
 } else if ($_SESSION["role"] == $_roleAdmin)
 {
-	include_once 'NavbarAdm.php';
-}
+	include_once 'NavbarAdm.php'; ?>
+	<div class="statContainer">
+  
+     
+<a class="nounderline"  title="Total Convicts">
+<div class="statBubbleContainer">
+<div class="statBubble">
+  <div class="statNum">
+  #<?php
+		$result = $conn->query($sqlTotalC);
+		$data=$result->fetch_assoc();
+		echo $data["TOTALC"];
+  ?>
+  </div>
+</div>
+  <h3>Number of convicts</h3>
+</div>
+</a>
+
+
+<a class="nounderline" title="Total users">
+<div class="statBubbleContainer">
+<div class="statBubble">
+  <div class="statNum">
+  #<?php
+		$result = $conn->query($sqlTotalU);
+		$data=$result->fetch_assoc();
+		echo $data["TOTALU"];
+  ?>
+  </div>
+</div>
+  <h3>Number of users</h3>
+</div>
+</a>
+  
+<a class="nounderline" title="Total visits">
+<div class="statBubbleContainer">
+<div class="statBubble">
+  <div class="statNum">
+  #<?php
+		$result = $conn->query($sqlTotalV);
+		$data=$result->fetch_assoc();
+		echo $data["TOTALV"];
+  ?>
+  </div>
+</div>
+  <h3>Number of visits</h3>
+</div>
+</a>
+  
+ <a class="nounderline" title="Total messages">
+<div class="statBubbleContainer">
+<div class="statBubble">
+  <div class="statNum">
+  #<?php
+		$result = $conn->query($sqlTotalM);
+		$data=$result->fetch_assoc();
+		echo $data["TOTALM"];
+  ?>
+  </div>
+</div>
+  <h3>Number of messages</h3>
+</div>
+</a>
+
+</div>
+
+	
+<?php }
 ?>
 
 
--- deton/index.php
+++ deton/index.php
@@ -1,13 +1,19 @@
 <?php
 session_start();
 include 'Connection.php';
-$display = false;
-$sqlAdm = "SELECT * FROM visit where status = 'P'";
+$displayAdm = false;
+$displayUsr = false;
+$displayBtn = false;
+$sqlAdm = "SELECT * FROM visit WHERE status = 'P' ORDER BY Visit_date DESC";
+$sqlUsr = "SELECT * FROM visit WHERE Username_id = '" . $_SESSION["id"] . "' ORDER BY Visit_date ASC";
 $resultAdm = $conn->query($sqlAdm);
+$resultUsr = $conn->query($sqlUsr);
 if($resultAdm->num_rows > 0) {
-	$display = true;
+	$displayAdm = true;
 }
-
+if($resultUsr->num_rows > 0) {
+	$displayUsr = true;
+}
 
 ?>
 
@@ -23,12 +29,85 @@ if($resultAdm->num_rows > 0) {
 <?php 
 include 'OwnFunctions.php';
 if ($_SESSION["role"] == $_roleClient) {
-	include_once 'Navbar.php';	?>
-	
-
-<?php } else if ($_SESSION["role"] == $_roleAdmin) {	
+	include_once 'Navbar.php';	
+	if ($displayUsr == true) { ?>
+	<div class="container2">
+	<h2 class="release-header">Curent requests</h2>
+	<form action="" method="post">
+	<!-- TABLE -->
+	<table class="table table-action">
+	  
+	  <thead>
+		<tr>
+		  <th class="t-small"></th>
+		  <th class="t-medium">Convict name</th>
+		  <th class="t-small">ID</th>
+		  <th class="t-medium">Relationship</th>
+		  <th class="t-medium">Visit type</th>
+		  <th class="t-medium">Objects</th>
+		  <th class="t-medium">Visit Date</th>
+		  <th class="t-small"></th>
+		</tr>
+	  </thead>
+	  
+	  <tbody>
+		
+		 <?php if ($resultUsr && $resultUsr->num_rows > 0) {
+					while($row = $resultUsr->fetch_assoc()) { ?>
+					  <tr>
+						 <td><?php if ($row["Status"] == "P") { $displayBtn = true; ?>
+						 <label><input name="num[]" type="checkbox" value="<?php echo $row['Visit_id'];?>"></label>
+						 <?php } ?></td>
+						 <td><?php echo $row["Convict_name"] ?></td>
+						 <td><?php echo $row["Convict_id"] ?></td>
+						 <td><?php echo $row["Relationship"] ?></td>
+						 <td><?php echo $row["Type_of_visit"] ?></td>
+						 <td><?php echo $row["Objects"] ?></td>
+						 <td><?php echo $row["Visit_date"] ?></td>
+						 <td><?php if ($row["Status"] == "Y") { ?> <img src="img/check.png" alt="Y"> 
+						 <?php } else if ($row["Status"] == "N") { ?>  <img src="img/uncheck.png" alt="N">
+						 <?php } else if ($row["Status"] == "P") { ?> <img src="img/pending.png" alt="P">
+						 <?php } ?></td>
+					  </tr>
+				 <?php
+						}
+					}
+					
+				 ?>
+		
+	  </tbody>
+	</table>
+	<!-- END TABLE -->
+	<?php if ($displayBtn == true) { ?>
+	<input type="submit" value="Remove" name="ren" id="delBtn" />
+	<?php } ?>
+	</form>
+		<?php
+		if(isset($_POST["ren"])) {
+			$box=$_POST["num"];
+			while(list($key,$val) = @each ($box)) {
+				$sql="UPDATE visit SET Status = 'N' WHERE Visit_id = '" . $val . "'";
+				if ($conn->query($sql) === TRUE) {
+				$ok=true;
+			} else {
+				echo "Error deleting record: " . $conn->error;
+			}
+			}
+		?>
+		<script type="text/javascript">
+		window.location.href=window.location.href;
+		</script>
+</div>
+		<?php
+		}
+	} else { ?> 
+	 <div class="container2">
+	 <h2 class="release-header" id="noReq">You don't have any pending requests!</h2>
+	 </div>
+	<?php }	
+} else if ($_SESSION["role"] == $_roleAdmin) {	
 	include_once 'NavbarAdm.php'; 
-	if ($display == true) { ?>
+	if ($displayAdm == true) { ?>
 	<div class="container2">
 	<h2 class="release-header">Curent requests</h2>
 	<form action="" method="post">
